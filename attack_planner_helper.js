06abe2cc863197665ff0237203f4cce8b317f16d
PaliKon <114577456+PaliKon@users.noreply.github.com>
2026-04-21 16:14:27 +0200

    Implement blocked time settings and validation functions
    Added functions for blocked time settings and time validation.

--- attack_planner_helper.js
+++ attack_planner_helper.js
@@ -1,7 +1,8 @@
-// Credits: b@ldr
+// Credits: b@ldr + custom blocked-time planning edit
 
 try {
 	var unitInfo;
+
 	jQuery
 		.ajax({
 			url: '/interface.php?func=get_unit_info',
@@ -10,18 +11,95 @@ try {
 			unitInfo = xml2json(jQuery(response));
 		});
 
+	function getBlockedTimeSettings() {
+		return {
+			enabled: jQuery('#exclude_blocked_attacks').is(':checked'),
+			from: jQuery('#blocked_from').val() || '00:00',
+			to: jQuery('#blocked_to').val() || '08:00',
+		};
+	}
+
+	function parseTimeToMinutes(timeStr) {
+		if (!timeStr || timeStr.indexOf(':') === -1) return null;
+
+		var parts = timeStr.split(':');
+		var hours = parseInt(parts[0], 10);
+		var minutes = parseInt(parts[1], 10);
+
+		if (
+			isNaN(hours) ||
+			isNaN(minutes) ||
+			hours < 0 ||
+			hours > 23 ||
+			minutes < 0 ||
+			minutes > 59
+		) {
+			return null;
+		}
+
+		return hours * 60 + minutes;
+	}
+
+	function isLaunchTimeBlocked(dateObj, blockedFrom, blockedTo) {
+		var fromMinutes = parseTimeToMinutes(blockedFrom);
+		var toMinutes = parseTimeToMinutes(blockedTo);
+
+		if (fromMinutes === null || toMinutes === null) {
+			return false;
+		}
+
+		var currentMinutes =
+			dateObj.getHours() * 60 + dateObj.getMinutes();
+
+		// napr. 01:00 -> 09:00
+		if (fromMinutes < toMinutes) {
+			return currentMinutes >= fromMinutes && currentMinutes < toMinutes;
+		}
+
+		// interval cez polnoc, napr. 23:00 -> 06:00
+		if (fromMinutes > toMinutes) {
+			return currentMinutes >= fromMinutes || currentMinutes < toMinutes;
+		}
+
+		// ak from == to, ber ako blokovaný celý deň
+		return true;
+	}
+
+	function isValidArrivalDate(dateString) {
+		var date = new Date(dateString);
+		return !isNaN(date.getTime());
+	}
+
 	function get_travel_times(attackers, defenders, speed, arrival_time) {
 		var travel_times = new Array();
+		var blockedSettings = getBlockedTimeSettings();
 
 		for (var i = 0; i < attackers.length; i++) {
-			attacker = attackers[i].split('|');
+			var attacker = attackers[i].split('|');
 			travel_times[attackers[i]] = new Array();
-			for (var j = 0; j < defenders.length; j++) {
-				defender = defenders[j].split('|');
-				distance = calculateDistance(attackers[i], defenders[j]);
 
-				// var currentTravelTime = distance * speed * 60;
-				var currentLaunchTime = fnCalculateLaunchTime(attackers[i], defenders[j], speed, arrival_time);
+			for (var j = 0; j < defenders.length; j++) {
+				var defender = defenders[j].split('|');
+				var distance = calculateDistance(attackers[i], defenders[j]);
+
+				var currentLaunchTime = fnCalculateLaunchTime(
+					attackers[i],
+					defenders[j],
+					speed,
+					arrival_time
+				);
+
+				if (
+					blockedSettings.enabled &&
+					isLaunchTimeBlocked(
+						currentLaunchTime,
+						blockedSettings.from,
+						blockedSettings.to
+					)
+				) {
+					continue;
+				}
+
 				travel_times[attackers[i]][defenders[j]] = currentLaunchTime.getTime();
 			}
 		}
@@ -30,19 +108,20 @@ try {
 	}
 
 	function get_plan(travel_times, max_attack, type) {
-		console.log(travel_times);
 		var plan = new Array();
 		var used_targets = new Array();
-		for (attack in travel_times) {
-			var fastest = 999999999999999999999.0;
+
+		for (var attack in travel_times) {
+			var fastest = Number.MAX_SAFE_INTEGER;
 			var target = '';
 			var travel_time = '';
 			plan[attack] = new Array();
-			for (defend in travel_times[attack]) {
-				0;
+
+			for (var defend in travel_times[attack]) {
 				if (typeof used_targets[defend] === 'undefined') {
 					used_targets[defend] = 0;
 				}
+
 				if (used_targets[defend] < max_attack) {
 					if (travel_times[attack][defend] < fastest) {
 						target = defend;
@@ -51,18 +130,22 @@ try {
 					}
 				}
 			}
-			if (target != '' && distance != '') {
+
+			if (target !== '' && travel_time !== '') {
 				used_targets[target] = used_targets[target] + 1;
 				plan[attack]['target'] = target;
 				plan[attack]['travel_time'] = travel_time;
 				plan[attack]['type'] = type;
 			}
 		}
+
 		return plan;
 	}
 
 	function get_troop(type) {
 		var unit = '';
+		var unitSpeed = '';
+
 		if (type == 'nobel') {
 			return '[unit]snob[/unit]';
 		} else if (type == 'nuke') {
@@ -74,6 +157,7 @@ try {
 		Object.entries(unitInfo.config).map((currentUnit) => {
 			if (currentUnit[1].speed === unitSpeed) {
 				unit = `[unit]${currentUnit[0]}[/unit]`;
+
 				if (type === 'nuke') {
 					if (currentUnit[0] === 'knight') {
 						unit = `[unit]light[/unit]`;
@@ -104,9 +188,16 @@ try {
 	function get_twcode(plan, land_time) {
 		var twcode = `[size=12][b]Landing time: ${land_time}[/b][/size]\n\n`;
 
+		if (!plan || !plan.length) {
+			return (
+				twcode +
+				'[b]No valid plans found with the current blocked send-time settings.[/b]'
+			);
+		}
+
 		var colour = '';
 
-		for (attack in plan) {
+		for (var attack in plan) {
 			if (
 				plan[attack]['target'] != undefined ||
 				plan[attack]['travel_time'] != undefined ||
@@ -121,9 +212,7 @@ try {
 				}
 
 				var launch_time = new Date(plan[attack]['travel_time']);
-
-				var formattedDate = launch_time.toString();
-				formattedDate = formatDateTime(formattedDate);
+				var formattedDate = formatDateTime(launch_time);
 
 				twcode +=
 					get_troop(plan[attack]['type']) +
@@ -143,7 +232,7 @@ try {
 	}
 
 	function merge(array1, array2) {
-		for (element in array2) {
+		for (var element in array2) {
 			if (typeof array1[element] === 'undefined') {
 				array1[element] = array2[element];
 			}
@@ -152,13 +241,16 @@ try {
 	}
 
 	function clean(clean_me, of_these) {
+		if (!clean_me) return null;
+		if (!of_these) return clean_me;
+
 		var cleaned = new Array();
-		for (element in clean_me) {
+		for (var element in clean_me) {
 			if (of_these.indexOf(clean_me[element]) == -1) {
 				cleaned.push(clean_me[element]);
 			}
 		}
-		return cleaned;
+		return cleaned.length ? cleaned : null;
 	}
 
 	function sort(array) {
@@ -166,32 +258,39 @@ try {
 		var sorted = new Array();
 		var keys = new Array();
 		var increment = 0.000000000001;
-		for (element in array) {
+
+		for (var element in array) {
 			if (typeof array[element]['travel_time'] !== 'undefined') {
+				var time;
 				if (typeof stored_by_time[array[element]['travel_time']] === 'undefined') {
-					var time = array[element]['travel_time'];
+					time = array[element]['travel_time'];
 				} else {
-					var time = array[element]['travel_time'] + increment;
+					time = array[element]['travel_time'] + increment;
 					increment += increment;
 				}
+
 				stored_by_time[time] = array[element];
 				stored_by_time[time]['attacker'] = element;
 			}
 		}
-		for (element in stored_by_time) {
-			keys.push(element);
+
+		for (var storedElement in stored_by_time) {
+			keys.push(storedElement);
 		}
-		keys.sort(function (b, a) {
-			return b - a;
+
+		keys.sort(function (a, b) {
+			return a - b;
 		});
-		for (key in keys) {
+
+		for (var key in keys) {
 			var plan = new Array();
-			(plan['attacker'] = stored_by_time[keys[key]]['attacker']),
-				(plan['target'] = stored_by_time[keys[key]]['target']),
-				(plan['type'] = stored_by_time[keys[key]]['type']),
-				(plan['travel_time'] = stored_by_time[keys[key]]['travel_time']);
+			plan['attacker'] = stored_by_time[keys[key]]['attacker'];
+			plan['target'] = stored_by_time[keys[key]]['target'];
+			plan['type'] = stored_by_time[keys[key]]['type'];
+			plan['travel_time'] = stored_by_time[keys[key]]['travel_time'];
 			sorted.push(plan);
 		}
+
 		return sorted;
 	}
 
@@ -200,54 +299,110 @@ try {
 
 		var arrival_time = jQuery('input#arrival_time').val();
 
+		if (!isValidArrivalDate(arrival_time)) {
+			alert('Invalid arrival time! Use format: yyyy-mm-dd hh:mm:ss');
+			return;
+		}
+
+		var blockedSettings = getBlockedTimeSettings();
+
+		if (blockedSettings.enabled) {
+			if (
+				parseTimeToMinutes(blockedSettings.from) === null ||
+				parseTimeToMinutes(blockedSettings.to) === null
+			) {
+				alert('Invalid blocked time range!');
+				return;
+			}
+		}
+
 		var nuke_speed = parseFloat(jQuery('select#nuke_unit').val());
 		var support_speed = parseFloat(jQuery('select#support_unit').val());
 		var nobel_speed = parseFloat(jQuery('input#nobleSpeed').val());
 
 		var nobel_coords = jQuery('textarea#nobel_coords').val().match(coord_regex);
+		var nuke_coords;
+		var support_coords;
 
 		if (nobel_coords == null) {
-			var nuke_coords = jQuery('textarea#nuke_coords').val().match(coord_regex);
+			nuke_coords = jQuery('textarea#nuke_coords').val().match(coord_regex);
 			if (nuke_coords == null) {
-				var support_coords = jQuery('textarea#support_coords').val().match(coord_regex);
+				support_coords = jQuery('textarea#support_coords').val().match(coord_regex);
 			} else {
-				var support_coords = clean(jQuery('textarea#support_coords').val().match(coord_regex), nuke_coords);
+				support_coords = clean(
+					jQuery('textarea#support_coords').val().match(coord_regex),
+					nuke_coords
+				);
 			}
 		} else {
-			var nuke_coords = clean(jQuery('textarea#nuke_coords').val().match(coord_regex), nobel_coords);
+			nuke_coords = clean(
+				jQuery('textarea#nuke_coords').val().match(coord_regex),
+				nobel_coords
+			);
+
 			if (nuke_coords == null) {
-				var support_coords = clean(jQuery('textarea#support_coords').val().match(coord_regex), nobel_coords);
+				support_coords = clean(
+					jQuery('textarea#support_coords').val().match(coord_regex),
+					nobel_coords
+				);
 			} else {
-				var support_coords = clean(
-					clean(jQuery('textarea#support_coords').val().match(coord_regex), nobel_coords),
+				support_coords = clean(
+					clean(
+						jQuery('textarea#support_coords').val().match(coord_regex),
+						nobel_coords
+					),
 					nuke_coords
 				);
 			}
 		}
 
 		var targets_coords = jQuery('textarea#target_coords').val().match(coord_regex);
-		var nuke_count = jQuery('input#nuke_count').val();
-		var support_count = jQuery('input#support_count').val();
-		var nobel_count = jQuery('input#nobel_count').val();
+		var nuke_count = parseInt(jQuery('input#nuke_count').val(), 10) || 0;
+		var support_count = parseInt(jQuery('input#support_count').val(), 10) || 0;
+		var nobel_count = parseInt(jQuery('input#nobel_count').val(), 10) || 0;
+
+		if (!targets_coords || !targets_coords.length) {
+			alert('Please enter target coordinates!');
+			return;
+		}
 
 		var all_plans = new Array();
 
 		jQuery('textarea#target_coords').val(targets_coords.join('\n'));
-		if (nobel_coords) {
-			var nobleTravelTimes = get_travel_times(nobel_coords, targets_coords, nobel_speed, arrival_time);
+
+		if (nobel_coords && nobel_coords.length) {
+			var nobleTravelTimes = get_travel_times(
+				nobel_coords,
+				targets_coords,
+				nobel_speed,
+				arrival_time
+			);
 			jQuery('textarea#nobel_coords').val(nobel_coords.join('\n'));
 			all_plans = merge(all_plans, get_plan(nobleTravelTimes, nobel_count, 'nobel'));
 		}
-		if (nuke_coords) {
-			var nukeTravelTimes = get_travel_times(nuke_coords, targets_coords, nuke_speed, arrival_time);
+
+		if (nuke_coords && nuke_coords.length) {
+			var nukeTravelTimes = get_travel_times(
+				nuke_coords,
+				targets_coords,
+				nuke_speed,
+				arrival_time
+			);
 			jQuery('textarea#nuke_coords').val(nuke_coords.join('\n'));
 			all_plans = merge(all_plans, get_plan(nukeTravelTimes, nuke_count, 'nuke'));
 		}
-		if (support_coords) {
-			var supportTravelTimes = get_travel_times(support_coords, targets_coords, support_speed, arrival_time);
+
+		if (support_coords && support_coords.length) {
+			var supportTravelTimes = get_travel_times(
+				support_coords,
+				targets_coords,
+				support_speed,
+				arrival_time
+			);
 			jQuery('textarea#support_coords').val(support_coords.join('\n'));
 			all_plans = merge(all_plans, get_plan(supportTravelTimes, support_count, 'support'));
 		}
+
 		all_plans = sort(all_plans);
 		jQuery('textarea#results').val(get_twcode(all_plans, arrival_time));
 	}
@@ -265,6 +420,9 @@ try {
 		currentMonth = currentMonth + 1;
 		currentMonth = '' + currentMonth;
 		currentMonth = currentMonth.padStart(2, '0');
+
+		currentDate = '' + currentDate;
+		currentDate = currentDate.padStart(2, '0');
 
 		currentHours = currentHours.padStart(2, '0');
 		currentMinutes = currentMinutes.padStart(2, '0');
@@ -332,7 +490,6 @@ try {
 
 	function fnCalculateLaunchTime(source, target, unitSpeed, landingTime) {
 		var distance = calculateDistance(target, source);
-
 		var landingTimeObject = new Date(landingTime);
 
 		const msPerSec = 1000;
@@ -343,7 +500,9 @@ try {
 
 		/* Truncate milli-second portion of the time */
 		var launchTime = new Date();
-		launchTime.setTime(Math.round((landingTimeObject.getTime() - unitTime) / msPerSec) * msPerSec);
+		launchTime.setTime(
+			Math.round((landingTimeObject.getTime() - unitTime) / msPerSec) * msPerSec
+		);
 
 		return launchTime;
 	}
